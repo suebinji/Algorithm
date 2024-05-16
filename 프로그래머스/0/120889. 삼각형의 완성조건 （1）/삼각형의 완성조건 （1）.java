ca5c49ce42b8e7b386915e1f674a80367bbe27d3
suebinji <110950766+suebinji@users.noreply.github.com>
2024-05-17 00:40:31 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.18 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,36 @@
+//import java.util.*;
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        
+       
+        int max = sides[0];
+        int min = sides[0];
+        int mid = sides[0];
+        
+        
+        for(int i = 0 ; i< sides.length; i++) {
+            if (max < sides[i]){
+                max = sides[i];
+            }
+            else if (min > sides[i]){
+                min = sides[i];
+            } 
+            else {
+                mid = sides[i];
+            }
+        }
+        
+        if (max >= (mid + min)) {
+            answer = 2;
+        } else {
+            answer = 1;
+        }
+        
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(mid);
+        
+        return answer;
+    }
+}
