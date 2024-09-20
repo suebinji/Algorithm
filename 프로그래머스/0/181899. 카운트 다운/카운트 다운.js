7e2a5835837430d2f958372a223a65c6abcc5ce8
suebinji <110950766+suebinji@users.noreply.github.com>
2024-09-20 14:55:20 +0900

    [level 0] Title: 카운트 다운, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -0,0 +1,11 @@
+function solution(start_num, end_num) {
+    var answer = [];
+    
+    while(start_num>=end_num) {
+        answer.push(start_num);
+        start_num--;
+    }
+    
+    
+    return answer;
+}
