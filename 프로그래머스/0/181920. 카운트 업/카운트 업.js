608c56d83e00cffda16a95fd1b4fa40844be628c
suebinji <110950766+suebinji@users.noreply.github.com>
2024-09-23 10:54:56 +0900

    [level 0] Title: 카운트 업, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,9 @@
+function solution(start_num, end_num) {
+    var answer = [];
+    
+    for (start_num; start_num <= end_num;start_num++) {
+        answer.push(start_num);
+    }
+    
+    return answer;
+}
