fb19e55424016825d805d902cc3fb12978bca133
suebinji <110950766+suebinji@users.noreply.github.com>
2024-09-24 20:54:24 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        
+        if (number % n == 0 && number % m ==0) {
+            return 1;
+        } else {
+            return 0;
+        }
+        
+        //return answer;
+    }
+}
