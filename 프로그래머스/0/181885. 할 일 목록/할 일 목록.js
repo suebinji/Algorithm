aca2f21cbc13479076f5d935f901764a323a5b25
suebinji <110950766+suebinji@users.noreply.github.com>
2024-09-24 10:35:19 +0900

    [level 0] Title: 할 일 목록, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,12 @@
+function solution(todo_list, finished) {
+    var answer = [];
+    
+    for (let i = 0 ; i< todo_list.length;i++) {
+        if (finished[i] == false) {
+            answer.push(todo_list[i]);
+        }
+        //answer = todo_list;
+    }
+    
+    return answer;
+}
