971adf51768e470bad3f241c9b89b93a9b5668cf
suebinji <110950766+suebinji@users.noreply.github.com>
2024-09-24 21:02:12 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.58 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[n];
+	    	
+	    	for(int i = 0; i < n ; i++) {
+	    		answer[i] = num_list[i];
+	    		
+	    		System.out.println(answer[i]);
+	    	}
+	    	
+	    	return answer;
+    }
+}
