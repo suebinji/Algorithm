3ac3501de72f9221d90e14e166d95a50b8572273
suebinji <110950766+suebinji@users.noreply.github.com>
2024-09-25 14:48:08 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.42 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    var answer = 1;
+    
+    for (let i =0 ; i < arr.length; i ++) {
+        for (let j = 0 ; j < i ; j++) {
+            if (arr[i][j] !== arr[j][i]) {
+                answer = 0;
+            }
+        }
+    }
+    
+    return answer;
+}
