8008a26cb5695a95125cbbb6e781320f2e3695c7
suebinji <110950766+suebinji@users.noreply.github.com>
2024-11-12 22:26:28 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,12 +1,10 @@
 -- 코드를 입력하세요
 
 select B.APNT_NO,	A.PT_NAME,	A.PT_NO, C.MCDP_CD, C.DR_NAME,	APNT_YMD 
-from PATIENT  A,
-APPOINTMENT  B ,
-DOCTOR  C
+from PATIENT  A
+join APPOINTMENT  B 
+on A.PT_NO = B.PT_NO
+join DOCTOR  C
+on C.DR_ID = B.MDDR_ID
 where to_char(APNT_YMD,'YYYY-MM-DD') = '2022-04-13'
 and apnt_cncl_yn= 'N'
-and C.MCDP_CD ='CS'
-and A.PT_NO = B.PT_NO
-and C.DR_ID = B.MDDR_ID
-order by APNT_YMD
